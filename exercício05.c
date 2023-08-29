6fddeb5f0d370ea24ac866960a0c42b34a006a8b
HenriqueMedeirosCosta <132111448+HenriqueMedeirosCosta@users.noreply.github.com>
2023-08-28 21:13:42 -0300

    Create exercício05.c

---
+++ exercício05.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+float numero1, numero2, numero3, div;
+  printf("Digite o valor do real\n");
+  scanf("%f", &numero1);
+  div = numero1 / 4.98;
+  printf("A conversão é: %.2f", div);
+
+}
