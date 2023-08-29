83749f8e7ce812de56a82f1e229aab81f7c017cc
HenriqueMedeirosCosta <132111448+HenriqueMedeirosCosta@users.noreply.github.com>
2023-08-28 21:12:43 -0300

    Create exercício03.c

---
+++ exercício03.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  int numero1, numero2, numero3;
+  int mult;
+
+  printf("Informe a base e a altura\n"); 
+  scanf(" %d %d", &numero1, &numero2);
+  mult = numero1 * numero2;
+  printf(" A área é: %d", mult);
+  
+  
+  
+  
+}
