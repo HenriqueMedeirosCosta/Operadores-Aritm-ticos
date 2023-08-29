701685d38ad4941b37de72a627f28f6378515da3
HenriqueMedeirosCosta <132111448+HenriqueMedeirosCosta@users.noreply.github.com>
2023-08-28 21:12:01 -0300

    Create exercicío02.c

---
+++ exercicío02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  int numero1, mult;
+  
+
+  printf("Digite um numero\n");
+  scanf(" %d", &numero1);
+  mult= numero1 * 2;
+  printf("O dobro é: %d", mult);
+  
+}
